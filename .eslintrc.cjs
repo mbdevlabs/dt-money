af420f857c7b3ad91dc4941491ba38482b3438c1
marcio ubuntu <mbreno.dev@gmail.com>
2025-12-20 17:38:32 -0300

    docs: update code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,25 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['@rocketseat/eslint-config/react'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+        endOfLine: 'lf',
+      },
+    ],
   },
-}
+};
